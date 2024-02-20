ebc398e921f57a87b431f998c8a3386791879924
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-02-20 15:39:55 +0100

    added pre-loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,30 @@
 import axios from "axios";
 import { useEffect, useRef } from "react";
 import { useState } from "react";
+import loader from "../assets/1481.gif";
 import { PiSpeakerSimpleHigh, PiSpeakerSimpleHighFill } from "react-icons/pi";
 
 const Home = () => {
   const [input, setInput] = useState("");
   const [search, setSearch] = useState(false);
   const [data, setData] = useState([]);
+  const [showRecent, setShowRecent] = useState(true);
+  const [loading, setLoading] = useState(true);
   const audioRef = useRef(null);
 
-  useEffect(() => {
-    console.log("first");
+  useEffect(() => { 
+   if(search){
+    setLoading(true)
     axios
       .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`)
       .then((res) => {
         setData(res.data);
+        console.log(res.data)
         setSearch(false);
-        console.log(res.data);
+        setLoading(false)
       })
       .catch((err) => console.log(err));
+   }
   }, [search]);
 
   const handleChange = (e) => {
@@ -61,41 +67,50 @@ const Home = () => {
           </i>
         </div>
       </div>
-      <div className={`${data ? "" : "hidden"}`}>
-        <ul className="flex flex-col gap-3 ">
-          {data?.map((words, index) => (
-            <li
-              className="flex flex-col p-2 bg-slate-50 text-slate-700 text-xs"
-              key={index}
-            >
-              <span className="text-base gap-2  flex  items-center capitalize">
-                {words.word} -
-                <span className="text-sm flex items-center font-semibold">
-                  {words.phonetic || words.phonetics[2].text} -{" "}
-                  {words.meanings[0].partOfSpeech}
+      {loading && data ? (
+        <div className=" flex items-center justify-center">
+          <img src={loader} alt="" />
+        </div>
+      ) : (
+        <div className={`${data ? "" : "hidden"}`}>
+          <ul className="flex flex-col gap-3 ">
+            {data?.map((words, index) => (
+              <li
+                className="flex flex-col p-2 bg-slate-50 text-slate-700 text-xs"
+                key={index}
+              >
+                <span className="text-base gap-2  flex  items-center capitalize">
+                  {words.word} -
+                  <span className="text-sm flex items-center font-semibold">
+                    {words.phonetic || words.phonetics[1].text || words.phonetics[2].text} -{" "}
+                    {words.meanings[0].partOfSpeech}
+                  </span>
+                  <audio
+                    ref={audioRef}
+                    src={words.phonetics[0].audio ?? words.phonetics[1].audio}
+                  ></audio>
+                  <button onClick={togglePlay}>
+                    <PiSpeakerSimpleHighFill />
+                  </button>
                 </span>
-                <audio
-                  ref={audioRef}
-                  src={words.phonetics[0].audio ?? words.phonetics[1].audio}
-                ></audio>
-                <button onClick={togglePlay}>
-                  <PiSpeakerSimpleHighFill />
-                </button>
-              </span>
-              {words.meanings[0].definitions[0].definition ??
-                words.meanings[0].definitions[1].definition ??
-                words.meanings[0].definitions[2].definition}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h3 className="text-xl font-bold mb-2">Recents</h3>
-        <ul className="">
-          <li>Bear</li>
-          <li>Transportat ion</li>
-        </ul>
-      </div>
+                {words.meanings[0].definitions[0].definition ??
+                  words.meanings[0].definitions[1].definition ??
+                  words.meanings[0].definitions[2].definition}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {showRecent && (
+        <div>
+          <h3 className="text-xl font-bold mb-2">Recents</h3>
+          <ul className="">
+            <li>Bear</li>
+            <li>Transportat ion</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
