ca36f0fff24be4b9e307810f5b947f93c4bedb48
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-02-20 17:01:33 +0100

    edits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,26 +61,6 @@ const Home = () => {
           onEnter={onEnter}
           setSearch={setSearch}
         />
-        {/* <div className="flex flex-col ">
-          <input
-            className="bg-gray-200 rounded-md h-9 pb-1 pl-3 mb-3"
-            onChange={handleChange}
-            onKeyDown={(e) => {
-              onEnter(e);
-            }}
-            type="text"
-            placeholder="search here"
-          />
-          <button
-            onClick={() => setSearch(true)}
-            className="bg-blue-500 w-fit text-white rounded-md px-3 py-1.5"
-          >
-            Search
-          </button>
-          <i className="text-gray-600 text-sm">
-            such as: sun, nature, programme
-          </i>
-        </div> */}
       </div>
       {loading ? (
         <div className="my-5 flex items-center justify-center">
