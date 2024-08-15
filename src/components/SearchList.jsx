7a144ea1a83fd4d9c03bde48a4e95061b11a691e
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-15 12:23:48 +0100

    refactor: Update SearchList component to include word truncation and phonetic display

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -2,19 +2,29 @@
 
 import { useNavigate } from "react-router-dom";
 
-const SearchList = ({ data }) => {
+const SearchList = ({ data, id }) => {
   const navigate = useNavigate();
 
   const handleWordClick = (word) => {
-    navigate(`/word/${word}`);
+    navigate(`/word/${word}/id/${id}`);
   };
+
+  const stringTruncate = (str, length) => {
+    const dots = str.length > length ? "..." : "";
+    return str.substring(0, length) + dots;
+  };
+  console.log(data);
 
   return (
     <li
-      onClick={()=>handleWordClick(data.word)}
-      className="bg-gray-100 px-2 rounded-md py-2"
+      onClick={() => handleWordClick(data.word)}
+      className="bg-gray-100 px-2 rounded-md py-2 flex justify-start gap-3 items-center cursor-pointer"
     >
-      {data.word}
+      <p> {data.word} </p>
+      <span className="flex text-xs w-fit gap-2">
+        <p>{stringTruncate(data.meanings[0].definitions[0].definition, 25)}</p>
+        <span className="font-bold">{data.phonetic}</span>
+      </span>
     </li>
   );
 };
