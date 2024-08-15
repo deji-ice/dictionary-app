c5160de348a4a9078bf111f51f00918d3ae79988
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-15 12:24:28 +0100

    feat: Add 404 Page Not Found route

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/word/:word/id/:id" element={<WordInfo />} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
       </Routes>
     </>
   );
